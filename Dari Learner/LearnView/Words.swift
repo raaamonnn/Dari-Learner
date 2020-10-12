832582d1653e69b9496d53c34f40d8dd0ae11e41
“raaamonnn” <ograaamonnn@gmail.com>
2020-10-11 20:01:27 -0700

    Update Words.swift
    Pretty much done with the API for now.
    Need to start adding logic to grab from the local api server.
    -get api database size
    -get 4 random words using that size(instead of doing it in the LearnModel do it in Words.swift)
    -pass on the data to the LearnModel.

--- Dari Learner/LearnView/Words.swift
+++ Dari Learner/LearnView/Words.swift
@@ -9,6 +9,33 @@
 import Foundation
 
 class Words{
+//    static var words:[Word] = {
+//        var request = URLRequest(url: URL("http://127.0.0.1:5000/words/all"))
+//        request.httpMethod = "GET"
+//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+//
+//        let session = URLSession.shared
+//        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+//            do {
+//                let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+//                if let respond = json.values.first {
+//                    DispatchQueue.main.async {
+//                        //it not a float and it will be a lot more objects
+//                        //also i need to only get 4 objects instead of all of them
+//                        //so i need to move the 4 random id logiv from LearnModel in here
+//                        let temp = respond as! String
+//                        let tempFloat = Float(temp)
+//                        self.label.text = String(format: "%.2f", tempFloat!)
+//                    }
+//                }
+//
+//            } catch {
+//                print("error")
+//            }
+//        })
+//
+//        task.resume()
+//        }
     static var words:[Word] = [Word(id: 0, EnglishWord: "Hello", DariWord: "Salaam"),Word(id: 1, EnglishWord: "Bread", DariWord: "Naan"),Word(id: 2, EnglishWord: "Thank You", DariWord: "Tashakor"),Word(id: 3, EnglishWord: "Yes", DariWord: "Baleh"), Word(id: 4, EnglishWord: "a", DariWord: "a"), Word(id: 5, EnglishWord: "b", DariWord: "b"), Word(id:6, EnglishWord: "c", DariWord: "c"), Word(id: 7, EnglishWord: "d", DariWord: "d")]
     
 }
